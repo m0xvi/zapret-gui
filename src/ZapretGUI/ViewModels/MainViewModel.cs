d80d37c8860c2c2c7a37636ffb0501702452d924
m0xvi <168997623+m0xvi@users.noreply.github.com>
2026-09-19 20:43:46 +0000

    Добавлен using System.Linq в MainViewModel для LINQ методов мониторинга
    Co-authored-by: arena-agent <297053741+arena-agent@users.noreply.github.com>

--- src/ZapretGUI/ViewModels/MainViewModel.cs
+++ src/ZapretGUI/ViewModels/MainViewModel.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.ObjectModel;
 using System.Diagnostics;
+using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
